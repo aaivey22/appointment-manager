441dc0931788a1a163f8cc991c75b03312d76216
aaivey22 <angelawings22@gmail.com>
2022-05-07 14:20:04 -0400

    initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,33 @@
+package main;
+
+/** Created class Main.java */
+/** @author Angela Ivey */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/** This class fires the appointment manager app.
+ * The Java Docs folder is located in the project directory. The folder is called "javadoc"
+ * FUTURE ENHANCEMENT the entire project should be translatable in multiple languages and also include enhancements for various disabilities.*/
+
+
+public class Main extends Application {
+    /** This is the start method that loads the landing page MainForm.*/
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //addTestData();
+        Parent root = FXMLLoader.load(getClass().getResource("/view/MainForm.fxml"));
+        primaryStage.setTitle("Appointment Management System");
+        primaryStage.setScene(new Scene(root, 1000, 600));
+        primaryStage.show();
+    }
+
+    /** This is the main method that launches the program.*/
+    public static void main(String[] args) {
+        launch(args);
+        System.out.println("reishi");
+    }
+}
