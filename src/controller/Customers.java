b925bd072a1e5cb7f480a6b7824f100204824d60
aaivey22 <angelawings22@gmail.com>
2022-05-26 14:28:55 -0400

    I created more fxml forms and their controller classes, next I will design them before creating the last batch which includes the report forms.

---
+++ src/controller/Customers.java
@@ -0,0 +1,14 @@
+package controller;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Customers implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
