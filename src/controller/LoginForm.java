4f1005ea70eae907c13f3aef823530808c482eb7
aaivey22 <angelawings22@gmail.com>
2022-06-13 15:32:30 -0400

    Completed translation of all text on the welcome form into french and english based on radio button choice. Language radio buttons are also functioning properly.

--- src/controller/LoginForm.java
+++ src/controller/LoginForm.java
@@ -1,11 +1,10 @@
 package controller;
 
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.control.RadioButton;
 
 import java.net.URL;
 import java.util.Locale;
@@ -23,12 +22,20 @@ public class LoginForm implements Initializable {
     public RadioButton freRadioButton;
     public RadioButton engRadioButton;
     public Label countryLabel;
+    public Label timeZoneLabel;
+    public Hyperlink registerLink;
+    public TextField userNameField;
+    public TextField passwordField;
+    public Label userNameLabel;
+    public Label passwordLabel;
+    public Hyperlink forgotPwordLink;
+    public Label selectLangLabel;
+    public Button loginButton;
 
     /** @param url,resourceBundle used to initialize translate() method.*/
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         translate();
-
     }
 
     /** @param actionEvent used to retrieve default language setting and set it to == the user's selected radio button.*/
@@ -43,12 +50,32 @@ public class LoginForm implements Initializable {
         /** @param actionEvent used to retrieve default language setting via Locale object to check if it == "fr" and will change it to === "en".*/
         if (Locale.getDefault().getLanguage().equals("fr")) {
             greeting.setText(rb.getString("greeting"));
+            freRadioButton.setText(rb.getString("freRadioButton"));
+            engRadioButton.setText(rb.getString("engRadioButton"));
             countryLabel.setText(rb.getString("countryLabel"));
-
-
+            timeZoneLabel.setText(rb.getString("timeZoneLabel"));
+            registerLink.setText(rb.getString("registerLink"));
+            userNameField.setText(rb.getString("userNameField"));
+            passwordField.setText(rb.getString("passwordField"));
+            userNameLabel.setText(rb.getString("userNameLabel"));
+            passwordLabel.setText(rb.getString("passwordLabel"));
+            forgotPwordLink.setText(rb.getString("forgotPwordLink"));
+            selectLangLabel.setText(rb.getString("selectLangLabel"));
+            loginButton.setText(rb.getString("loginButton"));
         } else if (Locale.getDefault().getLanguage().equals("en")) {
             greeting.setText(rb.getString("greeting"));
+            freRadioButton.setText(rb.getString("freRadioButton"));
+            engRadioButton.setText(rb.getString("engRadioButton"));
             countryLabel.setText(rb.getString("countryLabel"));
+            timeZoneLabel.setText(rb.getString("timeZoneLabel"));
+            registerLink.setText(rb.getString("registerLink"));
+            userNameField.setText(rb.getString("userNameField"));
+            passwordField.setText(rb.getString("passwordField"));
+            userNameLabel.setText(rb.getString("userNameLabel"));
+            passwordLabel.setText(rb.getString("passwordLabel"));
+            forgotPwordLink.setText(rb.getString("forgotPwordLink"));
+            selectLangLabel.setText(rb.getString("selectLangLabel"));
+            loginButton.setText(rb.getString("loginButton"));
         }
 
     }
