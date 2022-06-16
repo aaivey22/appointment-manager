a232011dbb4b87dd8a37e7f810f662b18ef752c1
aaivey22 <angelawings22@gmail.com>
2022-06-16 16:46:36 -0400

    Added variables to store both password fields' input on register form. Register button will now alert a user that attempts to register a new password that does not meet criteria.

--- src/controller/RegisterUser.java
+++ src/controller/RegisterUser.java
@@ -22,18 +22,39 @@ public class RegisterUser {
     public String newUserPassword;
     public String confirmUserPassword;
 
-
+    /**  @param actionEvent registerAction function used to execute warning alert to user if username is < 8 chars long. */
     public void registerAction(ActionEvent actionEvent) {
-        if (userNameField.getText().length() < 8) {
+        newUserName = userNameField.getText();
+        newUserPassword = passwordField.getText();
+        confirmUserPassword = confirmField.getText();
+
+        if (userNameField.getText().length() < 8 || userNameField.getText().length() > 12) {
             Alert alert = new Alert(Alert.AlertType.WARNING);
             alert.setResizable(true);
             alert.setTitle("Invalid User Name");
             alert.setContentText("User name must be between 8 - 12 characters long.");
             Optional<ButtonType> result = alert.showAndWait();
-
+        }
+        else {
+            if (passwordField.getText().length() < 8 || passwordField.getText().length() > 12) {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setResizable(true);
+                alert.setTitle("Invalid Password");
+                alert.setContentText("Password must be between 8 - 12 characters long.");
+                Optional<ButtonType> result = alert.showAndWait();
+            }
+            else {
+                if (newUserPassword.equals(confirmUserPassword)) {
+                    System.out.println("reishi");
+                }
+                else {
+                    System.out.println("no match");
+                }
+            }
         }
     }
 
+    /**  @param actionEvent cancelAction function used to redirect user to login form. */
     public void cancelAction(ActionEvent actionEvent) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/view/LoginForm.fxml"));
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
