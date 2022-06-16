9f3aeec9b45ce435f84dffc5f448d64f4bcd1728
aaivey22 <angelawings22@gmail.com>
2022-06-16 16:15:52 -0400

    Added variables to store user name field input on register form. Register button will now alert a user that attempts to register a new user name less than 8 characters long.

--- src/controller/RegisterUser.java
+++ src/controller/RegisterUser.java
@@ -5,18 +5,33 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class RegisterUser {
 
     public TextField userNameField;
     public TextField passwordField;
     public TextField confirmField;
+    public String newUserName;
+    public String newUserPassword;
+    public String confirmUserPassword;
+
 
     public void registerAction(ActionEvent actionEvent) {
+        if (userNameField.getText().length() < 8) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setResizable(true);
+            alert.setTitle("Invalid User Name");
+            alert.setContentText("User name must be between 8 - 12 characters long.");
+            Optional<ButtonType> result = alert.showAndWait();
+
+        }
     }
 
     public void cancelAction(ActionEvent actionEvent) throws IOException {
