6d9e846df9f80666be51d0b0c829da16a402f64e
aaivey22 <angelawings22@gmail.com>
2022-07-05 22:01:25 -0400

    Added fxml id's to fields and buttons on modify customers form.

--- src/controller/ModifyCustomers.java
+++ src/controller/ModifyCustomers.java
@@ -1,14 +1,40 @@
 package controller;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class ModifyCustomers implements Initializable {
+    public TextField custIDField;
+    public TextField custNameField;
+    public TextField custAddressField;
+    public TextField custPostalField;
+    public TextField custPhoneField;
+    public MenuButton custStateField;
+    public MenuButton custCountryField;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void saveChangesAction(ActionEvent actionEvent) {
+    }
+
+    public void revertChangesAction(ActionEvent actionEvent) {
+    }
+
+    public void custStateAction(ActionEvent actionEvent) {
+    }
+
+    public void custSCountryAction(ActionEvent actionEvent) {
+    }
+
+    public void directToCustomers(ActionEvent actionEvent) {
     }
 }
